2981714eefd60c6c6c95276dd10294b325552cf4
damon <gdamon@gmail.com>
2021-03-31 02:33:12 -0700

    add footer

---
+++ packages/react-app/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap/';
+import { SecondaryButton, ThemeButton, NCLogo, Blue } from '../components'
+
+export default function() {
+    return (
+        <Container fluid className="footer">
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
