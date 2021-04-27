923307ca4b8bc551f0b65bad62db237e356b1a68
KPrasch <kieranprasch@gmail.com>
2021-04-26 20:51:48 -0700

    Network stats module

---
+++ packages/react-app/src/components/stats.js
@@ -0,0 +1,46 @@
+import {Col, Container, Row} from 'react-bootstrap/';
+import {Blue, Grey, HR, Purple} from "./index";
+import React from "react";
+import {useQuery} from "@apollo/client";
+import {GET_LATEST_PERIOD} from "../graphql/subgraph";
+
+export default function NetworkStats() {
+
+    let {loading, error, data} = useQuery(GET_LATEST_PERIOD);
+    if (loading) return <p>Loading...</p>;
+    if (error) {
+        console.log(error);
+        return <p><i>There was a problem fetching the latest network status.</i></p>
+    }
+    const period = data.periods[0]
+    const locale = "en-US"
+    let supply = period.circulatingSupply ? Math.round(period.circulatingSupply).toLocaleString(locale) : "-"
+    let staked = period.totalStaked ? Math.round(period.totalStaked).toLocaleString(locale) : "-"
+    let stakers = period.activeStakers ? Number(period.activeStakers).toLocaleString(locale) : "-"
+
+    return (
+        <Row xs={1} md={3} className="d-flex justify-content-centered">
+            <Col className="d-flex justify-content-center">
+                <div>
+                    <HR color="blue"></HR>
+                    <h5><strong>Total NU Staked</strong></h5>
+                    <h2><strong><Blue>{staked}</Blue></strong></h2>
+                </div>
+            </Col>
+            <Col className="d-flex justify-content-center">
+                <div>
+                    <HR color="purple"></HR>
+                    <h5><strong>Circulating Supply</strong></h5>
+                    <h2><strong><Purple>{supply}</Purple></strong></h2>
+                </div>
+            </Col>
+            <Col className="d-flex justify-content-center">
+                <div>
+                    <HR color="grey75"></HR>
+                    <h5><strong>Active Stakers</strong></h5>
+                    <h2><strong><Grey>{stakers}</Grey></strong></h2>
+                </div>
+            </Col>
+        </Row>
+    )
+}
