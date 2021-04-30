25381fd16dc7fbda663a71b63d94c98d5f61b567
Kieran R. Prasch <kieranprasch@gmail.com>
2021-04-30 11:01:19 -0700

    Network graph formatting and denomination

--- packages/react-app/src/components/chart.js
+++ packages/react-app/src/components/chart.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 import {useQuery} from '@apollo/client';
-import {GET_PERIODS, GET_LATEST_PERIOD} from "../graphql/subgraph";
+import {GET_FINALIZED_PERIODS, GET_LATEST_FINALIZED_PERIOD} from "../graphql/subgraph";
 import {Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
+import {Context} from "../utils";
+import {PUBLIC_CHAINS} from "../constants";
+import {apolloClients} from "../graphql/apollo";
 
 
 const xFormatter = (s) => {
     return new Date(s * 1000).toLocaleDateString("en-US", {month: "short"}).toLowerCase();
+}
+
+function epochToDate(epoch) {
+    return new Date(epoch * 1000).toDateString("en-US")
 }
 
 class SupplyActiveDot extends React.Component {
@@ -15,6 +22,16 @@ class SupplyActiveDot extends React.Component {
         // console.log(this.props.payload.circulatingSupply)
         return (
             <circle cx={cx} cy={cy} r={4} stroke="#6B32D8" strokeWidth={2.5} fill="white"/>
+        );
+    }
+}
+
+class ActiveStakersActiveDot extends React.Component {
+    render() {
+        const {cx, cy} = this.props;
+        // console.log(this.props.payload.circulatingSupply)
+        return (
+            <circle cx={cx} cy={cy} r={4} stroke="#828A9C" strokeWidth={2.5} fill="white"/>
         );
     }
 }
@@ -36,6 +53,24 @@ class StakedActiveDot extends React.Component {
     }
 }
 
+function camelToTitleCase(text) {
+    let result = text.replace( /([A-Z])/g, " $1" );
+    return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+function round(value, decimals) {
+    if (decimals === undefined) decimals = 2
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function tooltipFormatter (value, name, props) {
+    if (name === "participationRate") {
+        value = value * 100
+    }
+    value = new Intl.NumberFormat('en').format(round(value))
+    return [value, camelToTitleCase(name)]
+}
+
 export default function StakerChart() {
 
     // Example Result
@@ -50,12 +85,26 @@ export default function StakerChart() {
     const utcMilllisecondsSinceEpoch = d.getTime() + (d.getTimezoneOffset() * 60 * 1000)
     const epoch = Number(Math.round(utcMilllisecondsSinceEpoch / 1000))
 
-    let {loading, error, data} = useQuery(GET_PERIODS, {variables: {epoch: epoch}});
+    const context = useContext(Context)
+    const {provider, account, web3} = context.wallet
+    let chainID;
+    if (provider) {
+        chainID = provider.chainId || 0
+    }
+
+    let client = apolloClients[chainID || 0]
+
+    let {loading, error, data} = useQuery(
+        GET_FINALIZED_PERIODS,
+        {variables: {epoch: epoch}, client: client}
+    );
+
     if (loading) return <p>Loading...</p>;
     if (error) {
-        console.log(error);
+        // console.log(error);
         return <p><i>There was a problem fetching the latest network status.</i></p>
     }
+
 
     return (
         <ResponsiveContainer aspect={2.38974359}>
@@ -75,7 +124,7 @@ export default function StakerChart() {
                     type="number"
                     scale="time"
                     tickSize={2}
-                    tick={{ transform: 'translate(80, 0)' }}
+                    tick={{ transform: 'translate(120, 0)' }}
                     tickCount={6}
                     interval={29}
                     domain={['dataMin', 'dataMax']}
@@ -91,9 +140,21 @@ export default function StakerChart() {
                        domain={[0, 5000]}
                 />
 
+                <YAxis hide="true"
+                       type="number"
+                       yAxisId={2}
+                       domain={[0, 1]}
+                />
+
                 <CartesianGrid horizontal={false} srroke="#E0E4E8"/>
 
-                <Tooltip/>
+                <Tooltip
+                    labelFormatter={epochToDate}
+                    formatter={tooltipFormatter}
+                    cursor={{ stroke: '#1E65F3', opacity: 0.5}}
+                    offset={15}
+                    wrapperStyle={{opacity: 0.9}}
+                />
 
                 <Area
                     dataKey="circulatingSupply"
@@ -103,7 +164,6 @@ export default function StakerChart() {
                     strokeWidth={2}
                     fillOpacity={0}
                     activeDot={<SupplyActiveDot/>}
-                    // stackId={0}
                 />
 
                 <Area
@@ -115,8 +175,6 @@ export default function StakerChart() {
                     fillOpacity={1}
                     fill="url(#colorPv)"
                     activeDot={<StakedActiveDot/>}
-                    // stackId={0}
-
                 />
 
                 <Area
@@ -127,6 +185,16 @@ export default function StakerChart() {
                     strokeWidth={2}
                     fillOpacity={0}
                     yAxisId={1}
+                    activeDot={<ActiveStakersActiveDot/>}
+                />
+
+                <Area
+                    dataKey="participationRate"
+                    type="monotone"
+                    unit="%"
+                    strokeWidth={0}
+                    fillOpacity={0}
+                    yAxisId={2}
                     activeDot={false}
                 />
             </AreaChart>
